a684ce33df085f200773572261bf10fe87b0816f
flux <adityavkd@gmail.com>
2012-08-25 13:18:29 +0530

    Updated project settings

---
+++ JBQA/JBQAParser.h
@@ -0,0 +1,13 @@
+//
+//  JBQAParser.h
+//  JBQA
+//
+//  Created by Aditya KD on 22/08/12.
+//  Copyright (c) 2012 Fr0st Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JBQAParser : NSObject
+
+@end
---
+++ JBQA/JBQAParser.m
@@ -0,0 +1,13 @@
+//
+//  JBQAParser.m
+//  JBQA
+//
+//  Created by Aditya KD on 22/08/12.
+//  Copyright (c) 2012 Fr0st Development. All rights reserved.
+//
+
+#import "JBQAParser.h"
+
+@implementation JBQAParser
+
+@end
