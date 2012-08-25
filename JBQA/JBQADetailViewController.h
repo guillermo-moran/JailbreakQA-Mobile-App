6efe51cd954fe8f277e38cb1e607d2c27d9d0430
flux <adityavkd@gmail.com>
2012-08-25 13:20:21 +0530

    Clean Up.

--- JBQA/JBQADetailViewController.h
+++ JBQA/JBQADetailViewController.h
@@ -8,21 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface JBQADetailViewController : UIViewController <UISplitViewControllerDelegate> {
-    
-    IBOutlet UIWebView* questionView;
-    IBOutlet UIImageView* avatarView;
-    IBOutlet UILabel* qAsker;
-    IBOutlet UITextView* qTitle;
-    
+@interface JBQADetailViewController : UIViewController <UISplitViewControllerDelegate>
+{
+    IBOutlet UIWebView *questionView;
+    IBOutlet UIImageView *avatarView;
+    IBOutlet UILabel *qAsker;
+    IBOutlet UITextView *qTitle;
 }
 
--(void)setQuestionContent:(NSString*)content;
--(void)setQuestionTitle:(NSString*)title asker:(NSString*)asker;
--(void)setAvatarFromURL:(NSURL*)url;
+-(void)setQuestionContent:(NSString *)content;
+-(void)setQuestionTitle:(NSString *)title asker:(NSString*)asker;
+-(void)setAvatarFromURL:(NSURL *)url;
 
 @property (strong, nonatomic) id detailItem;
-
 @property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
 
 @end
